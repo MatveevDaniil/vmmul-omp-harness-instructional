eda37cad7913adf05b8c4ccf315605fcffb8f7bd
MatveevDaniil <dmatveev@sfsu.edu>
2024-10-05 21:28:11 -0700

    FIX: inner loop test

--- dgemv-openmp-inner.cpp
+++ dgemv-openmp-inner.cpp
@@ -18,9 +18,9 @@ const char* dgemv_desc = "OpenMP dgemv.";
 */
 
 void my_dgemv(int n, double* A, double* x, double* y) {
-  for (int i = 0; i < n; i++)
-    #pragma omp parallel for
-    for (int j = 0; j < n; j++)
+  #pragma omp parallel for
+  for (int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++)
       #pragma omp atomic
       y[i] += A[i * n + j] * x[j];
 }
