f8d5e97be8b4d7f238c1517c058fb436f36722b2
MatveevDaniil <dmatveev@sfsu.edu>
2024-10-06 17:15:36 -0700

    FIX: thread reuse implementation

--- dgemv-openmp-inner-thread-reuse.cpp
+++ dgemv-openmp-inner-thread-reuse.cpp
@@ -8,11 +8,14 @@ const char* dgemv_desc = "OpenMP dgemv.";
 void my_dgemv(int n, double* A, double* x, double* y) {
   #pragma omp parallel 
   {
+    int thread_range = n / omp_get_num_threads();
+    int thread_id = omp_get_thread_num();
+    int start = thread_id * thread_range;
+    int end = start + thread_range;
     for (int i = 0; i < n; i++)
-    #pragma omp for
-    for (int j = 0; j < n; j++)
-      #pragma omp atomic
-      y[i] += A[i * n + j] * x[j];
+      for (int j = start; j < end; j++)
+        #pragma omp atomic
+        y[i] += A[i * n + j] * x[j];
   }
 }
 
