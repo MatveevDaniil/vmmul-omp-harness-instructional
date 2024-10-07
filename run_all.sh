b6f3aa51e79295b7e322cbb25006f5b8b6d60de6
MatveevDaniil <dmatveev@sfsu.edu>
2024-10-06 19:38:48 -0700

    FIX: run_all script

--- run_all.sh
+++ run_all.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 mkdir -p "./results/"
+global_start=$(date +%s)
 
 for source in basic vectorized blas
   do
+  local_start=$(date +%s)
   "./build/benchmark-$source" > "./results/$source.csv"
+  local_end=$(date +%s)
+  local_time=$(( local_end - local_start ))
+  global_time=$(( local_end - global_start ))
+  printf "runtime: %.1f,    global timer: %.1f\n" $local_time $global_time
 done
 
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-# inner inner-thread-reuse inner-reduction
-for source in outer inner-reduction
+for source in outer inner-reduction inner-thread-reuse inner-reduction
   do
   for t in 1 4 16 64
     do
     export OMP_NUM_THREADS=$t
-    echo "$source $t" 
+    local_start=$(date +%s)
     "./build/benchmark-openmp-$source" > "./results/openmp-$source.csv"
+    local_end=$(date +%s)
+    local_time=$(( local_end - local_start ))
+    global_time=$(( local_end - global_start ))
+    echo "$source $t" 
+    printf "runtime: %.1f,    global timer: %.1f\n" $local_time $global_time
   done
 done
