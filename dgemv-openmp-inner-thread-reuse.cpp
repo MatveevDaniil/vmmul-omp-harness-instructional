6d77c8f3288a5051d6c05e8e101f476f9dd6a8d2
MatveevDaniil <dmatveev@sfsu.edu>
2024-10-06 17:12:48 -0700

    FIX: thread reuse implementation

--- dgemv-openmp-inner-thread-reuse.cpp
+++ dgemv-openmp-inner-thread-reuse.cpp
@@ -9,7 +9,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
   #pragma omp parallel 
   {
     for (int i = 0; i < n; i++)
-    #pragma omp parallel for
+    #pragma omp for
     for (int j = 0; j < n; j++)
       #pragma omp atomic
       y[i] += A[i * n + j] * x[j];
