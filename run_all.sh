9065bbfb8da598ed0adb94ee27c83a5f726fca29
MatveevDaniil <dmatveev@sfsu.edu>
2024-10-06 19:21:06 -0700

    NEW: run_all script

---
+++ run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for source in basic vectorized blas
+  do
+  "./build/benchmark-$source" > "$source.csv"
+done
+
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+# inner inner-thread-reuse inner-reduction
+for source in outer inner-reduction
+  do
+  for t in 1 4 16 64
+    do
+    export OMP_NUM_THREADS=$t
+    echo "OMP_NUM_THREASDS = " $OMP_NUM_THREADS
+    "./build/benchmark-openmp-$source" > "openmp-$source.csv"
+  done
+done
